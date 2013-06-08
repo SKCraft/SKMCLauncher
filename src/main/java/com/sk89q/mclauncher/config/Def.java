61edd1a1387095e2ecd911696e818850091a7be4
Albert Pham <the.sk89q@gmail.com>
2013-06-07 21:42:50 -0700

    Changed setting 'launcher.reopen' to 'launcher.reopen-after-close' to force a default value change.

--- src/main/java/com/sk89q/mclauncher/config/Def.java
+++ src/main/java/com/sk89q/mclauncher/config/Def.java
@@ -32,7 +32,7 @@ public class Def {
     public static final String LAUNCHER_NO_NEWS = "launcher.newsless";
     public static final String LAUNCHER_HIDE_NEWS = "launcher.hidenews";
     public static final String LAUNCHER_ALWAYS_MORE_OPTIONS = "launcher.always-more-options";
-    public static final String LAUNCHER_REOPEN = "launcher.reopen";
+    public static final String LAUNCHER_REOPEN = "launcher.reopen-after-close";
     public static final String WINDOW_WIDTH = "window.width";
     public static final String WINDOW_HEIGHT = "window.height";
     public static final String WINDOW_FULLSCREEN = "window.fullscreen";
