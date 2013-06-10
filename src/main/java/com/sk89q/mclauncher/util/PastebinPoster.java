e38e2599a0958a6a3d63ef00305c95f792ef8944
Albert Pham <the.sk89q@gmail.com>
2013-06-09 19:52:19 -0700

    Fixed missing or incorrect file headers.

--- src/main/java/com/sk89q/mclauncher/util/PastebinPoster.java
+++ src/main/java/com/sk89q/mclauncher/util/PastebinPoster.java
@@ -1,7 +1,6 @@
-// $Id$
 /*
- * WorldGuard
- * Copyright (C) 2010 sk89q <http://www.sk89q.com>
+ * SK's Minecraft Launcher
+ * Copyright (C) 2010, 2011 Albert Pham <http://www.sk89q.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
